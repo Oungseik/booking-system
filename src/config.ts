5837b7008bd7768420a120b97becd85033ad4940
Oungseik <mhemaungthuwin@gmail.com>
2024-04-07 09:41:12 +0630

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+/**
+ * Import your all ENVIRONMENTAL VARIABLES here
+ */
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+	port: process.env.PORT || 8080,
+	NODE_ENV: process.env.NODE_ENV,
+};
+
+export default config;
