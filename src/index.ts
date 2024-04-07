7d64d3f543953c87678e5f257c344bea51d80346
Oungseik <mhemaungthuwin@gmail.com>
2024-04-07 11:18:39 +0630

    feat: setup mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import app from "./app";
 import config from "./config";
 import { logger } from "./lib/logger";
+import mongoose from "mongoose";
 
 app.listen(config.port, () => {
 	logger.info("server is running on port:", config.port);
+
+	mongoose
+		.connect("mongodb://localhost:27017/booking_system")
+		.then(() => {
+			logger.info("successfully connected to mongodb");
+		})
+		.catch((e) => {
+			logger.error("failed to connect to mongodb");
+			logger.error(e);
+		});
 });
